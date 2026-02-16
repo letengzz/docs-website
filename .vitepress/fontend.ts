9408be8c118a8fcc511805f76bd216a2a9615985
letengzz <2020885569@qq.com>
2026-02-16 20:24:31 +0800

    feat: 添加前端基础文档导航结构

--- .vitepress/fontend.ts
+++ .vitepress/fontend.ts
@@ -1,3 +1,36 @@
-export const FrontBasic = [];
+export const FrontBasic = [
+      {
+    text: "基础",
+    link: "/docs/Frontend/Basic/index.md",
+    items: [
+      { text: "前端开发", link: "/docs/Frontend/Basic/FrontendDevelop.md" },
+      {
+        text: "HTML", link: "/docs/Frontend/Basic/HTML/index.md", collapsed: true, items: [
+          { text: "HTML 概述", link: "/docs/Frontend/Basic/HTML/Overview/index.md" },
+          { text: "HTML 基础", link: "/docs/Frontend/Basic/HTML/Basic/index.md" },
+          { text: "HTML 语义化标签", link: "/docs/Frontend/Basic/HTML/Semantic/index.md" },
+          { text: "HTML 标签分类", link: "/docs/Frontend/Basic/HTML/Classify/index.md" },
+          { text: "HTML 全局属性", link: "/docs/Frontend/Basic/HTML/Global/index.md" },
+          { text: "HTML 排版标签", link: "/docs/Frontend/Basic/HTML/Compose/index.md" },
+          { text: "HTML 文本标签", link: "/docs/Frontend/Basic/HTML/Text/index.md" },
+          { text: "HTML 图片标签", link: "/docs/Frontend/Basic/HTML/Image/index.md" },
+          { text: "HTML 超链接标签", link: "/docs/Frontend/Basic/HTML/Hyperlink/index.md" },
+          { text: "HTML 视频标签", link: "/docs/Frontend/Basic/HTML/Video/index.md" },
+          { text: "HTML 音频标签", link: "/docs/Frontend/Basic/HTML/Audio/index.md" },
+          { text: "HTML 列表标签", link: "/docs/Frontend/Basic/HTML/List/index.md" },
+          { text: "HTML 表格标签", link: "/docs/Frontend/Basic/HTML/Table/index.md" },
+          { text: "HTML 表单标签", link: "/docs/Frontend/Basic/HTML/Form/index.md" },
+          { text: "HTML 框架标签", link: "/docs/Frontend/Basic/HTML/Iframe/index.md" },
+          { text: "HTML 实体标签", link: "/docs/Frontend/Basic/HTML/Entity/index.md" },
+          { text: "HTML meta元信息", link: "/docs/Frontend/Basic/HTML/meta/index.md" },
+          { text: "HTML 无语义标签", link: "/docs/Frontend/Basic/HTML/NoSemantic/index.md" },
+          { text: "HTML5 布局标签", link: "/docs/Frontend/Basic/HTML/Layout/index.md" },
+          { text: "HTML5 状态标签", link: "/docs/Frontend/Basic/HTML/Status/index.md" },
+          { text: "HTML5 兼容性处理", link: "/docs/Frontend/Basic/HTML/Compatibility/index.md" },
+        ]
+      }
+    ]
+  },
+];
 export const FrontFrame = [];
 export const FrontOthers = [];
