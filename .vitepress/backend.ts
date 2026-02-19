940625d46790338ad1e9f04520e7f1920a7a2a90
letengzz <2020885569@qq.com>
2026-02-19 18:10:19 +0800

    docs: 添加 .Net 相关文档链接和目录结构

--- .vitepress/backend.ts
+++ .vitepress/backend.ts
@@ -1,4 +1,28 @@
-export const DotNet = [];
+// .Net
+export const DotNet = [
+  {
+    text: ".Net",
+    link: "/docs/Backend/DotNet/index.md",
+    items: [
+      {
+        "link": "/docs/Backend/DotNet/Basic.md",
+        "text": "基础语法"
+      },
+      {
+        "link": "/docs/Backend/DotNet/Advanced.md",
+        "text": "进阶语法"
+      },
+      {
+        "link": "/docs/Backend/DotNet/DotNet5.md",
+        "text": ".Net5"
+      },
+      {
+        "link": "/docs/Backend/DotNet/WebApiNet6.md",
+        "text": "WebApi-Net6"
+      },
+    ]
+  }
+];
 export const Java = [];
 export const Go = [];
 export const Python = [];
